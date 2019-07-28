3a7fdbbbe2b0b61e331ae70f984e0c63293d7e09
wangyu2488 <414875346@qq.com>
2019-07-28 15:42:51 +0800

    修改人：首页接口提交

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import '../config/httpHeaders.dart';
+import '../service/service_method.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,49 +7,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String showText = '还没有请求数据';
+  String homePageContent = '正在获取数据';
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('极客时间'),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: _jike,
-                child: Text('请求数据'),
-              ),
-              Text(showText),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _jike(){
-    print('开始向极客请求数据......');
-    getHttp().then((val){
+  void initState() {
+    super.initState();
+    getHomePageContent().then((val) {
       setState(() {
-        showText=val['data'].toString();
+        homePageContent = val.toString();
       });
     });
   }
 
-  Future getHttp() async {
-    try {
-      Response response;
-      Dio dio = new Dio();
-      dio.options.headers = httpHeaders;
-      response=await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
-      print(response);
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('百姓生活+'),
+      ),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
+      ),
+    );
   }
 }
