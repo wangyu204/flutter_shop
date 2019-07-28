2845a24f7f1e2f46196cab97183d4c5c33f7be00
wangyu2488 <414875346@qq.com>
2019-07-28 10:19:46 +0800

    修改人：get函数提交

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,7 +7,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController;
+  TextEditingController typeController = TextEditingController();
   String showText = '欢迎来都美好人间';
 
   @override
@@ -15,29 +16,63 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('美好人间'),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: typeController,
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '商品类型',
-                  helperText: '请输入你喜欢的类型'),
-              autofocus: false,
-            ),
-            RaisedButton(
-              onPressed: null,
-              child: Text('选择完毕'),
-            ),
-            Text(
-              showText,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '商品类型',
+                    helperText: '请输入你喜欢的类型'),
+                autofocus: false,
+              ),
+              RaisedButton(
+                onPressed: _choiceAction,
+                child: Text('选择完毕'),
+              ),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _choiceAction() {
+    print('开始选择你喜欢的类型.....');
+    if (typeController.text.toString() == '') {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('商品类型不能会空'),
+        ),
+      );
+    } else {
+      getHttp(typeController.text.toString()).then((val) {
+        setState(() {
+          showText = val['data']['name'].toString();
+        });
+      });
+    }
+  }
+
+  Future getHttp(String typeText) async {
+    try {
+      Response response;
+      var data = {'name': typeText};
+      response = await Dio().get(
+        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
+        queryParameters: data,
+      );
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
