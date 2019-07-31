8bcdfd6aec93366cb127f5112638c42cea7c6d02
王喻 <wangyu@317hu.com>
2019-07-31 16:37:53 +0800

    修改内容：状态管理（感觉就是全局变量）

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:provide/provide.dart';
+import '../provide/counter.dart';
+
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('memberpage'),
+        child: Provide<Counter>(
+          builder: (context, child, counter) {
+            return Text(
+              '${counter.value}',
+              style: Theme.of(context).textTheme.display1,
+            );
+          },
+        ),
       ),
     );
   }
