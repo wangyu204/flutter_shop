363cea26c1051690458437b1dad7662cd9a443fa
王喻 <wangyu@317hu.com>
2019-07-31 14:17:16 +0800

    修改内容：分类界面接口调试成功提交

--- lib/config/service_url.dart
+++ lib/config/service_url.dart
@@ -2,4 +2,5 @@ const serviceUrl = 'https://wxmini.baixingliangfan.cn/baixing/';
 const servicePath = {
   'homePageContent': serviceUrl + 'wxmini/homePageContent', //商店首页信息
   'hotGoods': serviceUrl + 'wxmini/homePageBelowConten', //首页 火爆专区接口
+  'getCategory': serviceUrl + 'wxmini/getCategory', //分类 商品类别信息
 };
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,12 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CategoryPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+
+class CategoryPage extends StatefulWidget {
+  @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('category'),
+    _getCategory();
+
+    return Container(
+      child: Center(
+        child: Text('分类页面'),
       ),
     );
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      print('$data');
+    });
   }
 }
