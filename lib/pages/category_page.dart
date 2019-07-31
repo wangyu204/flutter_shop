3d54af6636d0d57b1eede1e284f6e362e03c53b8
王喻 <wangyu@317hu.com>
2019-07-31 16:05:34 +0800

    修改内容：商品分类页面左侧列表项实现

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../service/service_method.dart';
 import '../model/category.dart';
@@ -13,21 +14,18 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-    _getCategory();
-
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类'),
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
       ),
     );
-  }
-
-  void _getCategory() async {
-    await request('getCategory').then((val) {
-      var data = json.decode(val.toString());
-      CategoryModel list = CategoryModel.fromJson(data);
-      list.data.forEach((item) => print(item.mallCategoryName));
-    });
   }
 }
 
@@ -38,8 +36,64 @@ class LeftCategoryNav extends StatefulWidget {
 }
 
 class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  List<CategoryBigModel> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCategory();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+            right: BorderSide(
+          width: 1,
+          color: Colors.black12,
+        )),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12),
+          ),
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+
+      setState(() {
+        list = category.data;
+      });
+
+//      list.data.forEach((item) => print(item.mallCategoryName));
+    });
   }
 }
