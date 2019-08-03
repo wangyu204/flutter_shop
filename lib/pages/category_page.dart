772601e8dbb96cf68b458c78c9e03122d5fdeb43
王喻 <wangyu@317hu.com>
2019-08-03 10:07:42 +0800

    修改内容：Expanded解决控件溢出问题

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -200,14 +200,15 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
   Widget build(BuildContext context) {
     return Provide<CategoryGoodsListP>(
       builder: (context, child, data) {
-        return Container(
-          width: ScreenUtil().setWidth(570),
-          height: ScreenUtil().setHeight(1000),
-          child: ListView.builder(
-            itemCount: data.goodsList.length,
-            itemBuilder: (context, index) {
-              return _listItemWidget(data.goodsList, index);
-            },
+        return Expanded(
+          child: Container(
+            width: ScreenUtil().setWidth(570),
+            child: ListView.builder(
+              itemCount: data.goodsList.length,
+              itemBuilder: (context, index) {
+                return _listItemWidget(data.goodsList, index);
+              },
+            ),
           ),
         );
       },
