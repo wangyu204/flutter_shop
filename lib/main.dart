7e5746dd9afedc304002f1fc483e6016e768e4c6
王喻 <wangyu@317hu.com>
2020-01-09 16:29:44 +0800

    修改内容：页面导航跳转Navigator.push和Navigator.pop

--- lib/main.dart
+++ lib/main.dart
@@ -7,41 +7,46 @@ void main() {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: FirstPage(),
     ),
   );
 }
 
-class Home extends StatelessWidget {
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'helloe',
-          textAlign: TextAlign.center,
+        title: Text('first'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SecondPage()));
+          },
+          color: Colors.blue,
+          child: Text('second'),
         ),
       ),
-      body: Card(
-        child: Column(
-          //主轴居中，垂直方向
-          mainAxisAlignment: MainAxisAlignment.start, //最上面
-          //副轴,左对齐，水平方向
-          crossAxisAlignment: CrossAxisAlignment.start, //最左边
-          children: [
-            ListTile(
-              title: Text('1'),
-            ),
-            new Divider(),
-            ListTile(
-              title: Text('2'),
-            ),
-            new Divider(),
-            ListTile(
-              title: Text('3'),
-            ),
-            new Divider(),
-          ],
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('second'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          color: Colors.blue,
+          child: Text('back'),
         ),
       ),
     );
