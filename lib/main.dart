73508efdd2ae89f89dffa045f1ddc43b22cf043b
王喻 <wangyu@317hu.com>
2020-01-09 15:05:09 +0800

    修改内容：Stack层叠属性和positioned结合使用

--- lib/main.dart
+++ lib/main.dart
@@ -23,27 +23,32 @@ class Home extends StatelessWidget {
         ),
       ),
       body: Stack(
-        alignment: FractionalOffset(0.5, 1),
         children: <Widget>[
-          CircleAvatar(
-            backgroundImage: NetworkImage(
-                'http://7xtqgc.com1.z0.glb.clouddn.com/FmmbTsaBQBZ-W0fgKMYrqPMvlO1D'),
-            //圆半径大小r，直接就是2*r
-            radius: 100,
-          ),
           Container(
+            width: 375,
+            height: 500,
             // 盒子样式
             decoration: new BoxDecoration(
-              color: Colors.blue,
+              color: Colors.white,
               //设置Border属性给容器添加边框
               border: new Border.all(
                 //为边框添加颜色
-                color: Colors.blue,
+                color: Colors.white,
                 //边框宽度
                 width: 1,
               ),
             ),
             child: Text('wangyu'),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            width: 375,
+            child: RaisedButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text('底部'),
+            ),
           ),
         ],
       ),
