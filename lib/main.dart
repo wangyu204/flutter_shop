502510af24b252b8a1334439a8043ab1bc0248c6
王喻 <wangyu@317hu.com>
2020-01-10 17:17:11 +0800

    修改内容：单个支持伸缩的控件 ExpansionTile实现

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,46 +17,23 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  List<Widget> list = [];
-
-
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final height = MediaQuery
-        .of(context)
-        .size
-        .height;
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('wrap'),
+        title: Text('当个支持收缩的控件'),
       ),
-      body: Center(
-        child: Container(
-          //宽度
-          width: width,
-          //高度
-          height: height / 2.0,
-          // 盒子样式
-          decoration: new BoxDecoration(
-            color: Colors.grey,
-            //设置Border属性给容器添加边框
-            border: new Border.all(
-              //为边框添加颜色
-              color: Colors.grey,
-              //边框宽度
-              width: 0,
-            ),
-          ),
-          child: Wrap(
-            children: list,
-            spacing: 8.0,
-          ),
-        ),
+      body: ExpansionTile(
+        title: Text('Expansion Tile'),
+        leading: Icon(Icons.ac_unit),
+        backgroundColor: Colors.white12,
+        children: <Widget>[
+          ListTile(
+            title: Text('list tile'),
+            subtitle: Text('subtitle'),
+          )
+        ],
+        initiallyExpanded: true,
       ),
     );
   }
@@ -66,7 +42,6 @@ class HomePageState extends State<HomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    list..add(_buildAddButton());
   }
 
   @override
@@ -85,61 +60,5 @@ class HomePageState extends State<HomePage> {
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-  }
-
-  Widget _buildAddButton() {
-    return GestureDetector(
-      onTap: () {
-        if (list.length < 9) {
-          //一定要放在这里添加不然不生效
-          setState(() {
-            list.insert(list.length - 1, _buildPhoto());
-          });
-        }
-      },
-      child: Container(
-        //宽度
-        width: 80,
-        //高度
-        height: 80,
-        margin: EdgeInsets.all(8),
-        // 盒子样式
-        decoration: new BoxDecoration(
-          color: Colors.yellow,
-          //设置Border属性给容器添加边框
-          border: new Border.all(
-            //为边框添加颜色
-            color: Colors.yellow,
-            //边框宽度
-            width: 0,
-          ),
-        ),
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-
-  Widget _buildPhoto() {
-    return Container(
-      //宽度
-      width: 80,
-      //高度
-      height: 80,
-      margin: EdgeInsets.all(8),
-      alignment: Alignment.center,
-      // 盒子样式
-      decoration: new BoxDecoration(
-        color: Colors.yellow,
-        //设置Border属性给容器添加边框
-        border: new Border.all(
-          //为边框添加颜色
-          color: Colors.yellow,
-          //边框宽度
-          width: 0,
-        ),
-      ),
-      child: Text('text'),
-    );
   }
 }
