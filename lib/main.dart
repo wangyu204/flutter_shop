b71095d24f1dbef664ddb52dcb2068b2592f54ba
王喻 <wangyu@317hu.com>
2020-01-10 15:52:43 +0800

    修改内容：本地通用搜索栏实现

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop1/pages/home_page.dart';
+
+final searchList = ['1', '111', '121', '2', '222'];
+final recentSuggest = ['1', '2'];
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'hello',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstPage(),
+  runApp(MaterialApp(
+    title: '搜索栏',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
     ),
-  );
+    home: HomePage(),
+  ));
 }
 
-class FirstPage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('first'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () async {
-            final result = await Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondPage()));
-            print('${result}');
-          },
-          color: Colors.blue,
-          child: Text('second'),
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('搜索栏'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: () {
+            showSearch(context: context, delegate: HomePageSearchDelegate());
+          }),
+        ],
       ),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
+class HomePageSearchDelegate extends SearchDelegate<String> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('second'),
-      ),
-      body: Center(
-        child: RaisedButton(
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
           onPressed: () {
-            Navigator.pop(context, 'wangyu');
-          },
-          color: Colors.blue,
-          child: Text('back'),
+            query = '';
+          }),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final itemList = query.isEmpty
+        ? recentSuggest
+        : searchList.where((input) => input.startsWith(query)).toList();
+    return ListView.builder(
+        itemCount: itemList.length,
+        itemBuilder: (context, index) =>
+            ListTile(
+              title: RichText(
+                  text: TextSpan(
+                      text: itemList[index].substring(0, query.length),
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                      children: [
+                        TextSpan(
+                            text: itemList[index].substring(query.length),
+                            style: TextStyle(color: Colors.grey))
+                      ])),
+            ));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      //宽度
+      width: 100,
+      //高度
+      height: 100,
+      // 盒子样式
+      decoration: new BoxDecoration(
+        color: Colors.green,
+        //设置Border属性给容器添加边框
+        border: new Border.all(
+          //为边框添加颜色
+          color: Colors.green,
+          //边框宽度
+          width: 0,
         ),
       ),
+      child: Text('${query}'),
     );
   }
 }
